eef0039438151859ba3e7ba50a30e10f54e6ebe0
shangxianw <shangxianw123>
2020-07-01 21:20:59 +0800

    1

--- client/src/common/MovieClip.ts
+++ client/src/common/MovieClip.ts
@@ -1,36 +1,31 @@
-module war
+export class MovieClip extends egret.MovieClip
 {
-	export class MovieClip extends egret.MovieClip
+	public currAction:string;
+	public constructor()
 	{
-		public currAction:string;
-		public constructor()
-		{
-			super();
-		}
+		super();
+	}
 
-		public initData(fileName:string, clipName:string)
-		{
-			// 很危险。
-			// ResManager.Ins().loadRes(`${fileName}_json`);
-			// ResManager.Ins().loadRes(`${fileName}_png`);
-			let data = RES.getRes(`${fileName}_json`);
-			let txtr = RES.getRes(`${fileName}_png`);
-			let mcFactory:egret.MovieClipDataFactory = new egret.MovieClipDataFactory(data, txtr);
-			this.movieClipData = mcFactory.generateMovieClipData(clipName);
-		}
+	public initData(fileName:string, clipName:string)
+	{
+		// 很危险。
+		let data = RES.getRes(`${fileName}_json`);
+		let txtr = RES.getRes(`${fileName}_png`);
+		let mcFactory:egret.MovieClipDataFactory = new egret.MovieClipDataFactory(data, txtr);
+		this.movieClipData = mcFactory.generateMovieClipData(clipName);
+	}
 
-		public destroy()
-		{
-			
-		}
+	public destroy()
+	{
+		
+	}
 
-		public startPlay(action:string, count:number)
+	public startPlay(action:string, count:number)
+	{
+		if(this.currAction != action)
 		{
-			if(this.currAction != action)
-			{
-				this.currAction = action;
-				this.gotoAndPlay(action, count);
-			}
+			this.currAction = action;
+			this.gotoAndPlay(action, count);
 		}
 	}
 }
