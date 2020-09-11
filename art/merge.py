a22f2f89da52607c474415ef1b3546977706cd8d
shangxianw <shangxianw123>
2020-09-11 19:22:35 +0800

    111

--- art/merge.py
+++ art/merge.py
@@ -1,10 +1,163 @@
+import os
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-img = Image.open("bg_card_di_1.png")
-print(img.size[0])
-color = [255, 0, 0, 0]
-image = Image.new('RGBA', (1024, 1024), (color[0],color[1],color[2], color[3]))
-image.paste(img, (0, 0))
-image.save("2.png")
+class Merge(object):
+    def __init__(self):
+        super(Merge, self).__init__()
+        self.init()
+    
+    def init(self):
+        self.oriDir = "./资源/"
+        self.tarDir = "./导出资源/"
+        self.maxWidth = 1024
+        self.canvasWidth = 0
+        self.canvasHeight = 0
+
+        self.hasImgDirArray = [] # 记录该文件夹下是否有非文件夹
+
+        # 属性名
+        self.name = "name"
+        self.x = "x"
+        self.y = "y"
+        self.w = "w"
+        self.h = "h"
+        self.offX = "offX"
+        self.offY= "offY"
+        self.sourceW = "sourceW"
+        self.sourceH = "sourceH"
+        self.texture = "texture"
+    
+    def start(self):
+        self.openFolder(self.oriDir)
+        self.startMerge()
+    
+    def openFolder(self, pFilePath):
+        fileArray = os.listdir(pFilePath)
+        for file in fileArray:
+            filePath = pFilePath + file
+            if os.path.isdir(filePath) is True:
+                filePath += "/"
+                self.openFolder(filePath)
+            else:
+                if self.hasSamePFilePath(pFilePath) is False:
+                    self.hasImgDirArray.append(pFilePath)
+    
+    def startMerge(self):
+        for dir in self.hasImgDirArray:
+            array = self.initAllImgInfo(dir)
+            if len(array) > 0:
+                self.calcCanvas(array)
+                self.save(array, dir)
+    
+    # 初始化每张图片的信息并保存在数组中
+    def initAllImgInfo(self, dir):
+        fileArray = os.listdir(dir)
+        imgInfoArray = []
+        for file in fileArray:
+            path = dir + file
+            if os.path.isfile(path) is False:
+                continue
+            filename, type = file.split(".")
+            if type == ".png" or type == ".jpg":
+                imgInfo = {}
+                texture = Image.open(path)
+                imgInfo[self.texture] = texture
+                imgInfo[self.name] = filename
+                imgInfo[self.x] = 0
+                imgInfo[self.y] = 0
+                imgInfo[self.w] = texture.size[0]
+                imgInfo[self.h] = texture.size[1]
+                imgInfo[self.offX] = 0
+                imgInfo[self.offY] = 0
+                imgInfo[self.sourceW] = texture.size[0]
+                imgInfo[self.sourceH] = texture.size[1]
+                imgInfoArray.append(imgInfo)
+                # print(imgInfo)
+        return imgInfoArray
+    
+    # 计算每张图片的位置 & 画布宽高
+    def calcCanvas(self, imgInfoArray):
+        self.canvasWidth = 0
+        self.canvasHeight = 0
+        for i in range(len(imgInfoArray)):
+            curr = imgInfoArray[i]
+            x = curr[self.x]
+            y = curr[self.y]
+            endX = curr[self.x] + curr[self.w]
+            endY = curr[self.y] + curr[self.h]
+
+            curr[self.x] = self.canvasWidth
+            curr[self.y] = y
+
+            if endX > self.canvasWidth:
+                self.canvasWidth = endX
+            if endY > self.canvasHeight:
+                self.canvasHeight = endY
+    
+    def save(self, imgInfoArray, dir):
+        image = Image.new("RGBA", (self.canvasWidth, self.canvasHeight), (255, 0, 0, 0))
+        jsonContent =  '{\n'
+        jsonContent += '    "file":"[fileName]",\n'
+        jsonContent += '    "frames":\n'
+        jsonContent += '    {\n'
+        jsonContent += '[content]'
+        jsonContent += '    }\n'
+        jsonContent += '}'
+        content = ""
+        for imgInfo in imgInfoArray:
+            texture = imgInfo[self.texture]
+            image.paste(texture, box=(imgInfo[self.x], imgInfo[self.y]))
+            tmp = '        "[name]":{"x":[x], "y":[y], "w":[w], "h":[h], "offX":[offX], "offY":[offY], "sourceW":[sourceW], "sourceH":[sourceH]},\n'
+            tmp = tmp.replace("[name]", imgInfo[self.name])
+            tmp = tmp.replace("[x]", str(imgInfo[self.x]))
+            tmp = tmp.replace("[y]", str(imgInfo[self.y]))
+            tmp = tmp.replace("[w]", str(imgInfo[self.w]))
+            tmp = tmp.replace("[h]", str(imgInfo[self.h]))
+            tmp = tmp.replace("[offX]", str(imgInfo[self.offX]))
+            tmp = tmp.replace("[offY]", str(imgInfo[self.offY]))
+            tmp = tmp.replace("[sourceW]", str(imgInfo[self.sourceW]))
+            tmp = tmp.replace("[sourceH]", str(imgInfo[self.sourceH]))
+            content += tmp
+        jsonContent = jsonContent.replace("[content]", content)
+        jsonContent = jsonContent.replace(",\n    }\n", "\n    }\n")
+        # print(jsonContent)
+    
+        dir = dir.replace(self.oriDir, self.tarDir)
+        array = dir.split("/")
+        savaDir = dir + "../"
+        saveName = array[len(array)-2]
+        pngName = savaDir + saveName + ".png"
+        jsonName = savaDir + saveName + ".json"
+        jsonContent = jsonContent.replace("[fileName]", saveName + ".png")
+
+        if not os.path.exists(savaDir):
+            os.makedirs(savaDir)
+        image.save(pngName)
+        with open(jsonName, "w", encoding="utf-8") as f:
+            f.write(jsonContent)
+
+    def destroy(self):
+        pass
+    
+    def hasSamePFilePath(self, newPath):
+        try:
+            self.hasImgDirArray.index(newPath)
+            return True
+        except(BaseException):
+            return False
+    
+    def removeAllFile(self):
+        pass
+
+M = Merge()
+M.start()
+M.destroy()
+# img = Image.open("bg_card_di_1.png")
+# img2 = Image.open("heroicon_10010.png")
+# color = [255, 0, 0, 0]
+# image = Image.new('RGBA', (1024, 1024), (color[0],color[1],color[2], color[3]))
+# image.paste(img, (0, 0), img) 
+# image.paste(img2, (100, 100), img2) 
+# image.save("2.png")
